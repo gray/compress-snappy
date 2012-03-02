29ce16d35effde45cfa9a3aeab1ac2c1e96d6e02
gray <gray@cpan.org>
2012-03-01 20:30:50 -0800

    copy updated benchmark script from Compress::LZ4

--- ex/benchmark.pl
+++ ex/benchmark.pl
@@ -6,8 +6,8 @@ use Benchmark qw(timethese);
 use Getopt::Long qw(GetOptions :config no_ignore_case);
 use List::Util qw(max);
 
-# Install Task::Digest
 use Compress::Bzip2  ();
+use Compress::LZ4    ();
 use Compress::LZF    ();
 use Compress::Snappy ();
 use Compress::Zlib   ();
@@ -18,31 +18,41 @@ my %opts = (
 );
 GetOptions(\%opts, 'iterations|i=i', 'size|s=f',);
 
-my $data = '0' x (1_024 * $opts{size});
+my $data = join '', ('A'..'Z', 'a'..'z', 0..9, qw(_ .)) x (16 * $opts{size});
 
 my %compress = (
     'Compress::Bzip2::compress'  => sub { Compress::Bzip2::compress($data) },
+    'Compress::LZ4::compress'    => sub { Compress::LZ4::compress($data) },
     'Compress::LZF::compress'    => sub { Compress::LZF::compress($data) },
     'Compress::Snappy::compress' => sub { Compress::Snappy::compress($data) },
     'Compress::Zlib::compress'   => sub { Compress::Zlib::compress($data) },
 );
 
 my $bzip2  = Compress::Bzip2::compress($data);
+my $lz4    = Compress::LZ4::compress($data);
 my $lzf    = Compress::LZF::compress($data);
 my $snappy = Compress::Snappy::compress($data);
 my $zlib   = Compress::Zlib::compress($data);
 
+my %compression_ratios = (
+    'Compress::Bzip2::compress'  => length($bzip2) / length($data),
+    'Compress::LZ4::compress'    => length($lz4) / length($data),
+    'Compress::LZF::compress'    => length($lzf) / length($data),
+    'Compress::Snappy::compress' => length($snappy) / length($data),
+    'Compress::Zlib::compress'   => length($zlib) / length($data),
+);
+
 my %decompress = (
     'Compress::Bzip2::decompress' =>
         sub { Compress::Bzip2::decompress($bzip2) },
+    'Compress::LZ4::decompress' => sub { Compress::LZ4::decompress($lz4) },
     'Compress::LZF::decompress' => sub { Compress::LZF::decompress($lzf) },
     'Compress::Snappy::decompress' =>
         sub { Compress::Snappy::decompress($snappy) },
-    'Compress::Zlib::uncompress' =>
-        sub { Compress::Zlib::uncompress($zlib) },
+    'Compress::Zlib::uncompress' => sub { Compress::Zlib::uncompress($zlib) },
 );
 
-run(\%compress, 'Compressible', 'compression');
+run(\%compress, 'Compressible', 'compression', \%compression_ratios);
 print "\n";
 run(\%decompress, 'Compressible', 'decompression');
 print "\n";
@@ -53,11 +63,20 @@ $data = join '', map { chr int rand(256) } (1 .. $opts{size});
 $data = Compress::Bzip2::compress($data);
 
 $bzip2  = Compress::Bzip2::compress($data);
+$lz4    = Compress::LZ4::compress($data);
 $lzf    = Compress::LZF::compress($data);
 $snappy = Compress::Snappy::compress($data);
 $zlib   = Compress::Zlib::compress($data);
 
-run(\%compress, 'Uncompressible', 'compression');
+%compression_ratios = (
+    'Compress::Bzip2::compress'  => length($bzip2) / length($data),
+    'Compress::LZ4::compress'    => length($lz4) / length($data),
+    'Compress::LZF::compress'    => length($lzf) / length($data),
+    'Compress::Snappy::compress' => length($snappy) / length($data),
+    'Compress::Zlib::compress'   => length($zlib) / length($data),
+);
+
+run(\%compress, 'Uncompressible', 'compression', \%compression_ratios);
 print "\n";
 run(\%decompress, 'Uncompressible', 'decompression');
 
@@ -65,7 +84,7 @@ exit;
 
 
 sub run {
-    my ($tests, $type, $op) = @_;
+    my ($tests, $type, $op, $c_ratios) = @_;
 
     my $header = sprintf '%s data (%s KiB) - %s', $type, $opts{size}, $op;
     printf "%s\n%s\n", $header, '-' x length($header);
@@ -74,6 +93,11 @@ sub run {
 
     my @info;
     my ($max_name_len, $max_rate_len, $max_bw_len) = (0, 0, 0);
+    my ($c_format, $c_ratio_sub) = ('', sub { });
+    if ($c_ratios) {
+        $c_format    = '  %.3f%%';
+        $c_ratio_sub = sub { 100 * $c_ratios->{$_[0]} };
+    }
 
     while (my ($name, $info) = each %$times) {
         my ($duration, $cycles) = @{$info}[ 1, 5 ];
@@ -93,7 +117,7 @@ sub run {
 
         my $name_padding = $max_name_len - length($name);
 
-        printf "%s %s %${max_rate_len}s/s  %${max_bw_len}s MiB/s\n",
-            $name, ' 'x$name_padding, $rate, $bw;
+        printf "%s %s %${max_rate_len}s/s  %${max_bw_len}s MiB/s$c_format\n",
+            $name, ' 'x$name_padding, $rate, $bw, $c_ratio_sub->($name);
     }
 }
