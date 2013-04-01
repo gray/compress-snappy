d7f1cfe8c7561353856bb00e4b19234f09a670f2
gray <gray@cpan.org>
2013-03-31 20:43:27 -0700

    update copyright dates

--- lib/Compress/Snappy.pm
+++ lib/Compress/Snappy.pm
@@ -147,7 +147,7 @@ L<http://search.cpan.org/dist/Compress-Snappy/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011-2012 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2011-2013 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
